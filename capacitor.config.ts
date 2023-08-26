f7127d5a9155656ca2ccc43ff90d8606d66c7000
Carlos Abarca Vega <soporte@compuelec.cl>
2023-08-26 12:21:13 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AppLocalBusiness',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
